58ed3a1c3d5c3c01042caf56a003821b69b52957
T4Z3R07 <ethangandiya@gmail.com>
2025-11-11 21:52:43 +0200

    Implement cocktails section

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
+import Cocktails from './components/Cocktails.jsx';
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText); 
@@ -12,6 +13,7 @@ const App = () => {
     <main>
         <Navbar />
         <Hero />
+        <Cocktails />
     </main>
   )
 }
